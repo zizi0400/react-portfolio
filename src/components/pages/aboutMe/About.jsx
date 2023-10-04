155b9115f6bfd1f9fec7f3c2952bdab1645ee485
dci-student <zizi.0400@gmail.com>
2023-10-04 20:00:46 +0200

    changed the Paragraph about me

--- src/components/pages/aboutMe/About.jsx
+++ src/components/pages/aboutMe/About.jsx
@@ -9,34 +9,31 @@ function About() {
         <h1>A B O U T M E</h1>
         <img src={line} alt="" />
         <p>
-          Hallo! Ich bin Zahra. Ich bin ein Frontend Developer mit einjähriger
-          Erfahrung in diesem Bereich.
+          Hello! I'm Zahra. I am a one year old frontend developer Experience in
+          this area.
           <br />
-          Seit meiner Teenager interessierte ich mich sehr für die Welt der
-          Computer und Technik. <br />
-          Dieses Interesse hat mich schon in jungen Jahren für das Programmieren
-          interessiert. <br /> Mein erster Einstieg in die Programmierung
-          erfolgte über einfachere Sprachen wie HTML und CSS, die es mir
-          ermöglichten, einfache Websites zu erstellen.
+          Since I was a teenager, I have been very interested in the world of
+          Computers and technology. <br />
+          This interest made me interested in programming at a young age
+          interested. <br /> My first introduction to programming was done via
+          simpler languages like HTML and CSS, which worked for me made it
+          possible to create simple websites.
           <br />
-          Programmieren ist für mich mehr als eine Leidenschaft. <br />
-          Es ist eine Möglichkeit, Veränderungen herbeizuführen und reale
-          Probleme zu lösen.
+          Programming is more than a passion for me. <br />
+          It's a way to make change and real change To solve problems.
           <br />
-          Außerdem bin ich immer auf der Suche nach neuen Technologien und der
-          Verbesserung meiner Programmierkenntnisse, damit ich zu neuen und
-          interessanten Projekten für die Computerindustrie und die Gesellschaft
-          beitragen kann. <br />
-          Wenn ich nicht bei der Arbeit bin, entwerfe und experimentiere ich
-          gerne mit anderen Funktionen, die interessant sein könnten. <br />
-          Mein Ziel ist Förderung und Verbesserung und ich bin immer bereit für
-          neue Herausforderungen und die Zusammenarbeit mit verschiedenen
-          Menschen im Einklang mit diesem Ziel. <br />
-          Wenn Sie mit mir zusammenarbeiten oder sich mit mir vernetzen möchten,
-          würde ich gerne mehr hören. <br />
+          I'm also always looking for new technologies and the like Improving my
+          programming skills so I can learn new and interesting projects for the
+          computer industry and society can contribute. <br />
+          When I'm not at work, I'm designing and experimenting happy with other
+          functions that might be interesting. <br />
+          My goal is promotion and improvement and I am always ready for that
+          new challenges and collaboration with different people people in
+          harmony with this goal. <br />
+          If you would like to collaborate or network with me, I would like to
+          hear more. <br />
           <br />
-          Vielen Dank, dass Sie sich die Zeit genommen haben, mehr über mich zu
-          erfahren.
+          Thank you for taking the time to find out more about me experience.
         </p>
       </div>
     </div>
